b85187601aa1af011d10150ac3a2f0f9e68e0509
Dmitrii Ladnov <ladnov.dmitry@yandex.ru>
2024-02-10 16:12:10 +0300

    Update pom.xml Add AppConfig.java Add User.java Add db.properties

---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,41 @@
+package web.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users_table")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "id")
+    private Long id;
+//    @Column(name = "name")
+    private String firstName;
+//    @Column(name = "last_name")
+    private String lastName;
+
+
+    public User() {
+    }
+
+    public User(String name, String lastName) {
+        this.firstName = name;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
